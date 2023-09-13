96822e546266666acd79cc31edeb8e21a6330093
cisarpati <125928993+cisarpati@users.noreply.github.com>
2023-09-13 12:34:41 -0500

    CRUD con javaScript

---
+++ CRUD _ JavaScript/BACKEND/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Peticion GET');
+});
+
+app.post('/', (req, res)=>{
+    res.send('Peticion POST');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor encendido');
+});
